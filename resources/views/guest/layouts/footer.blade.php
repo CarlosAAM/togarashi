6169e5a3aaf34d44af8f9e50cecb3dc39ed3983e
Carlos2396 <carlosaam96@gmail.com>
2018-06-14 17:14:16 -0500

    Only text is missing from main page.

--- resources/views/guest/layouts/footer.blade.php
+++ resources/views/guest/layouts/footer.blade.php
@@ -4,148 +4,43 @@
         <div class="row">
             <div class="col-sm-6 col-md-4 p-t-50">
                 <!-- - -->
-                <h4 class="txt13 m-b-33">
-                    Contact Us
-                </h4>
+                <h4 class="txt13 m-b-33">Contáctanos</h4>
 
                 <ul class="m-b-70">
                     <li class="txt14 m-b-14">
                         <i class="fa fa-map-marker fs-16 dis-inline-block size19" aria-hidden="true"></i>
-                        8th floor, 379 Hudson St, New York, NY 10018
+                        Av. las Margaritas 527, El Patrimonio, 72450 Puebla, Pue.
                     </li>
 
                     <li class="txt14 m-b-14">
                         <i class="fa fa-phone fs-16 dis-inline-block size19" aria-hidden="true"></i>
-                        (+1) 96 716 6879
+                        01 222 375 3024
                     </li>
 
                     <li class="txt14 m-b-14">
                         <i class="fa fa-envelope fs-13 dis-inline-block size19" aria-hidden="true"></i>
-                        contact@site.com
+                        admin@togarashi.com.mx
                     </li>
                 </ul>
+            </div>
 
+            <div class="col-sm-6 col-md-4 p-t-50">
                 <!-- - -->
                 <h4 class="txt13 m-b-32">
-                    Opening Times
+                    Horarios de apertura
                 </h4>
 
                 <ul>
-                    <li class="txt14">
-                        09:30 AM – 11:00 PM
-                    </li>
+                    <li class="txt14">15:00 – 21:00</li>
 
-                    <li class="txt14">
-                        Every Day
-                    </li>
+                    <li class="txt14">Martes a Sábado</li>
+
+                    <li class="txt14">14:00 – 19:00</li>
+
+                    <li class="txt14">Domingo</li>
                 </ul>
             </div>
 
-            <div class="col-sm-6 col-md-4 p-t-50">
-                <!-- - -->
-                <h4 class="txt13 m-b-33">
-                    Latest twitter
-                </h4>
-
-                <div class="m-b-25">
-                    <span class="fs-13 color2 m-r-5">
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                    </span>
-                    <a href="#" class="txt15">
-                        @colorlib
-                    </a>
-
-                    <p class="txt14 m-b-18">
-                        Activello is a good option. It has a slider built into that displays the featured image in the slider.
-                        <a href="#" class="txt15">
-                            https://buff.ly/2zaSfAQ
-                        </a>
-                    </p>
-
-                    <span class="txt16">
-                        21 Dec 2017
-                    </span>
-                </div>
-
-                <div>
-                    <span class="fs-13 color2 m-r-5">
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                    </span>
-                    <a href="#" class="txt15">
-                        @colorlib
-                    </a>
-
-                    <p class="txt14 m-b-18">
-                        Activello is a good option. It has a slider built into that displays
-                        <a href="#" class="txt15">
-                            https://buff.ly/2zaSfAQ
-                        </a>
-                    </p>
-
-                    <span class="txt16">
-                        21 Dec 2017
-                    </span>
-                </div>
-            </div>
-
-            <div class="col-sm-6 col-md-4 p-t-50">
-                <!-- - -->
-                <h4 class="txt13 m-b-38">
-                    Gallery
-                </h4>
-
-                <!-- Gallery footer -->
-                <div class="wrap-gallery-footer flex-w">
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-01.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-01.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-02.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-02.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-03.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-03.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-04.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-04.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-05.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-05.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-06.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-06.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-07.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-07.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-08.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-08.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-09.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-09.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-10.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-10.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-11.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-11.jpg') }}" alt="GALLERY">
-                    </a>
-
-                    <a class="item-gallery-footer wrap-pic-w" href="images/photo-gallery-12.jpg" data-lightbox="gallery-footer">
-                        <img src="{{ asset('assets/pato/images/photo-gallery-thumb-12.jpg') }}" alt="GALLERY">
-                    </a>
-                </div>
-
-            </div>
         </div>
     </div>
 
@@ -153,9 +48,7 @@
         <div class="container">
             <div class="flex-sb-m flex-w p-t-22 p-b-22">
                 <div class="p-t-5 p-b-5">
-                    <a href="#" class="fs-15 c-white"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
-                    <a href="#" class="fs-15 c-white"><i class="fa fa-facebook m-l-18" aria-hidden="true"></i></a>
-                    <a href="#" class="fs-15 c-white"><i class="fa fa-twitter m-l-18" aria-hidden="true"></i></a>
+                        <a href="https://www.facebook.com/Togarashi-Japanese-Food-354601284626002/" target="_blank"><i class="fa fa-facebook m-l-18" aria-hidden="true"></i></a>
                 </div>
 
                 <div class="txt17 p-r-20 p-t-5 p-b-5">
